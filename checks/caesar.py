819bb854f695b70f5699d99dd6685223ad7b4ee4
hljames <haileyjames@college.harvard.edu>
2017-06-19 09:57:15 -0400

    fixed names of tests

--- checks/caesar.py
+++ checks/caesar.py
@@ -2,9 +2,9 @@ import os
 import sys
 
 sys.path.append(os.getcwd())
-from check50 import File, Test, Error, check
+from check50 import File, TestCase, Error, check
 
-class Vigenere(Test):
+class Caesar(TestCase):
 
     @check()
     def exists(self):
@@ -17,31 +17,31 @@ class Vigenere(Test):
         self.spawn("clang -o caesar caesar.c -lcs50").exit(0)
 
     @check("compiles")
-    def test_1a(self):
+    def encrypts_a_as_b(self):
         """encrypts "a" as "b" using 1 as key"""
         self.spawn("./caesar 1").stdin("a").stdout("ciphertext:\s*b\n", "ciphertext: b").exit(0)
 
     @check("compiles")
-    def test_23barfoo(self):
+    def encrypts_barfoo_as_yxocll(self):
         """encrypts "barfoo" as "yxocll" using 23 as key"""
         self.spawn("./caesar 23").stdin("barfoo").stdout("ciphertext:\s*yxocll\n", "ciphertext: yxocll").exit(0)
 
     @check("compiles")
-    def test_3BARFOO(self):
+    def encrypts_BARFOO_as_EDUIRR(self):
         """encrypts "BARFOO" as "EDUIRR" using 3 as key"""
         self.spawn("./caesar 3").stdin("BARFOO").stdout("ciphertext:\s*EDUIRR\n", "ciphertext: EDUIRR").exit(0)
 
     @check("compiles")
-    def test_4BaRFoo(self):
+    def encrypts_BaRFoo_FeVJss(self):
         """encrypts "BaRFoo" as "FeVJss" using 4 as key"""
         self.spawn("./caesar 4").stdin("BaRFoo").stdout("ciphertext:\s*FeVJss\n", "ciphertext: FeVJss").exit(0)
 
     @check("compiles")
-    def test_65barfoo(self):
+    def encrypts_barfoo_as_onesbb(self):
         """encrypts "barfoo" as "onesbb" using 65 as key"""
         self.spawn("./caesar 65").stdin("barfoo").stdout("ciphertext:\s*onesbb\n", "ciphertext: onesbb").exit(0)
 
     @check("compiles")
-    def test_noarg(self):
+    def handles_no_argv(self):
         """handles lack of argv[1]"""
         self.spawn("./caesar").exit(1)
