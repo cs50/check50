48489799d718759fbdf8a086c9260282d6b8cf41
hljames <haileyjames@college.harvard.edu>
2017-06-27 14:48:30 -0400

    pset6 checks complete

--- checks/cs50/2017/x/sentiments/checks.py
+++ checks/cs50/2017/x/sentiments/checks.py
@@ -25,163 +25,10 @@ class Sentiments(TestCase):
     def server_down(self):
         self.driver.close()
         self.server.kill()
-    #
-    # def override_lookup(self):
-    #     """Replaces student's lookup function with a deterministic one."""
-    #     self.append_code("helpers.py", File("resources/finance/lookup.py"))
-    #
-    # def register(self, username, password, confirmation, outcome):
-    #     """Checks that registering results in success or failure."""
-    #
-    #     # go to register page
-    #     self.driver.get("{}/register".format(Finance.BASE_PATH))
-    #     fields = self.driver.find_elements_by_tag_name("input")
-    #
-    #     # check for input fields
-    #     if len(fields) == 0:
-    #         raise Error("No available input fields to register.")
-    #
-    #     # fill in name, password, confiramtion
-    #     fields_filled = 0
-    #     for field in fields:
-    #         name = field.get_attribute("name")
-    #         if name == "username":
-    #             fields_filled += 1
-    #             field.send_keys(username)
-    #         elif name == "password":
-    #             fields_filled += 1
-    #             field.send_keys(password)
-    #         elif name == "confirmation":
-    #             fields_filled += 1
-    #             field.send_keys(confirmation)
-    #     if fields_filled != 3:
-    #         raise Error("Could not fill in username and/or password.")
-    #
-    #     # search for register button
-    #     buttons = self.driver.find_elements_by_tag_name("button")
-    #     register_button = None
-    #     for button in buttons:
-    #         if button.text == "Register":
-    #             register_button = button
-    #     if register_button == None:
-    #         raise Error("Could not find register button.")
-    #     register_button.click()
-    #
-    #     if "Apology" in self.driver.title and outcome:
-    #         raise Error("Expected registration success, not rejection.")
-    #     elif "Apology" not in self.driver.title and not outcome:
-    #         raise Error("Expected registration rejection, not acceptance.")
-    #
-    # def login(self, username, password):
-    #     """Checks that user can log in."""
-    #
-    #     # go to login page
-    #     self.driver.get("{}/login".format(Finance.BASE_PATH))
-    #     fields = self.driver.find_elements_by_tag_name("input")
-    #     fields_filled = 0
-    #     for field in fields:
-    #         name = field.get_attribute("name")
-    #         if name == "username":
-    #             fields_filled += 1
-    #             field.send_keys(username)
-    #         elif name == "password":
-    #             fields_filled += 1
-    #             field.send_keys(password)
-    #     if fields_filled != 2:
-    #         raise Error("Could not fill in username and/or password.")
-    #
-    #     # search for log in button
-    #     buttons = self.driver.find_elements_by_tag_name("button")
-    #     login_button = None
-    #     for button in buttons:
-    #         if button.text == "Log In":
-    #             login_button = button
-    #     if login_button == None:
-    #         raise Error("Could not find log in button.")
-    #     login_button.click()
-    #
-    #     if "Apology" in self.driver.title:
-    #         raise Error("Expected login to succeed, not fail.")
-    #
-    # def quote(self, ticker, outcome, value=None):
-    #     """Checks that getting a quote results in desired outcome."""
-    #
-    #     # go to quote page
-    #     self.driver.get("{}/quote".format(Finance.BASE_PATH))
-    #     fields = self.driver.find_elements_by_tag_name("input")
-    #
-    #     # find field to input quote
-    #     quote_field = None
-    #     for field in fields:
-    #         name = field.get_attribute("name")
-    #         if name == "symbol":
-    #             quote_field = field
-    #     if quote_field == None:
-    #         raise Error("Could not input quote.")
-    #     quote_field.send_keys(ticker)
-    #
-    #     # search for quote button
-    #     buttons = self.driver.find_elements_by_tag_name("button")
-    #     quote_button = None
-    #     for button in buttons:
-    #         if button.text == "Quote":
-    #             quote_button = button
-    #     if quote_button == None:
-    #         raise Error("Could not find quote button.")
-    #     quote_button.click()
-    #
-    #     if "Apology" in self.driver.title and outcome:
-    #         raise Error("Expected quote success, not failure.")
-    #     elif "Apology" not in self.driver.title and not outcome:
-    #         raise Error("Expected quote failure, not success.")
-    #     elif outcome and value:
-    #         # check the output against the given value
-    #         if str(value) not in self.driver.find_element_by_tag_name("body").text:
-    #             raise Error("Expected quote not found.")
-    #
-    # def transaction(self, transaction, symbol, shares, outcome, value=None, remaining=None):
-    #     """Checks that purchasing shares results in given outcome."""
-    #
-    #     # go to buy page
-    #     self.driver.get("{}/{}".format(Finance.BASE_PATH, transaction))
-    #     fields = self.driver.find_elements_by_tag_name("input")
-    #
-    #     # fill in fields
-    #     fields_filled = 0
-    #     for field in fields:
-    #         name = field.get_attribute("name")
-    #         if name == "symbol":
-    #             fields_filled += 1
-    #             field.send_keys(symbol)
-    #         elif name == "shares":
-    #             fields_filled += 1
-    #             field.send_keys(shares)
-    #     if fields_filled != 2:
-    #         raise Error("Could not fill in symbol and/or shares.")
-    #
-    #     # search for buy button
-    #     buttons = self.driver.find_elements_by_tag_name("button")
-    #     buy_button = None
-    #     for button in buttons:
-    #         if button.text == transaction.capitalize():
-    #             buy_button = button
-    #     if buy_button == None:
-    #         raise Error("Could not find {} button.".format(transaction))
-    #     buy_button.click()
-    #
-    #     if "Apology" in self.driver.title and outcome:
-    #         raise Error("Expected {} success, not failure.".format(transaction))
-    #     elif "Apology" not in self.driver.title and not outcome:
-    #         raise Error("Expected {} failure, not success.".format(transaction))
-    #
-    #     # go to index and check balance
-    #     if outcome and (value or remaining):
-    #         self.driver.get(Finance.BASE_PATH)
-    #         if value and not re.search(value, self.driver.page_source):
-    #             raise Error("Expected to see value {}.".format(value))
-    #         if remaining and not re.search(remaining, self.driver.page_source):
-    #             raise Error("Expected to see cash of value {}.".format(remaining))
 
+    def override_get_user_timeline(self):
+        """Replaces student's get_user_timeline function with a deterministic one."""
+        self.append_code("helpers.py", File("get_user_timeline.py"))
 
     @check()
     def exists_application(self):
@@ -193,12 +40,17 @@ class Sentiments(TestCase):
         """smile exists."""
         super(Sentiments, self).exists("smile")
 
+    @check()
+    def exists_tweets(self):
+        """tweets exists."""
+        super(Sentiments, self).exists("tweets")
+
     @check("exists_smile")
     def smile_love(self):
         """love returns :)"""
         self.include("positive-words.txt")
         self.include("negative-words.txt")
-        out = self.spawn("python3 smile love").stdout()
+        out = self.spawn("./smile love").stdout()
         stripped = Sentiments.ansi_escape.sub('', out).rstrip()
         if (stripped != ":)"):
             raise Error("love did not return :)")
@@ -208,7 +60,7 @@ class Sentiments(TestCase):
         """hate returns :("""
         self.include("positive-words.txt")
         self.include("negative-words.txt")
-        out = self.spawn("python3 smile hate").stdout()
+        out = self.spawn("./smile hate").stdout()
         stripped = Sentiments.ansi_escape.sub('', out).rstrip()
         if (stripped != ":("):
             raise Error("hate did not return :(")
@@ -218,7 +70,7 @@ class Sentiments(TestCase):
         """jellyfish returns :|"""
         self.include("positive-words.txt")
         self.include("negative-words.txt")
-        out = self.spawn("python3 smile jellyfish").stdout()
+        out = self.spawn("./smile jellyfish").stdout()
         stripped = Sentiments.ansi_escape.sub('', out).rstrip()
         if (stripped != ":|"):
             raise Error("jellyfish did not return :|")
@@ -228,7 +80,7 @@ class Sentiments(TestCase):
         """LOVE returns :)"""
         self.include("positive-words.txt")
         self.include("negative-words.txt")
-        out = self.spawn("python3 smile LOVE").stdout()
+        out = self.spawn("./smile LOVE").stdout()
         stripped = Sentiments.ansi_escape.sub('', out).rstrip()
         if (stripped != ":)"):
             raise Error("LOVE did not return :)")
@@ -238,142 +90,79 @@ class Sentiments(TestCase):
         """hAte returns :("""
         self.include("positive-words.txt")
         self.include("negative-words.txt")
-        out = self.spawn("python3 smile hAte").stdout()
+        out = self.spawn("./smile hAte").stdout()
         stripped = Sentiments.ansi_escape.sub('', out).rstrip()
         if (stripped != ":("):
             raise Error("hAte did not return :(")
 
     @check("exists_smile")
     def smile_no_args(self):
-        """handles lack of arg"""
+        """smile handles lack of arg"""
         self.include("positive-words.txt")
         self.include("negative-words.txt")
-        self.spawn("python3 smile").exit(1)
+        self.spawn("./smile").exit(1)
 
     @check("exists_smile")
     def smile_too_many_args(self):
-        """handles too many args"""
+        """smile handles too many args"""
         self.include("positive-words.txt")
         self.include("negative-words.txt")
-        self.spawn("python3 smile love hate").exit(1)
+        self.spawn("./smile love hate").exit(1)
 
 
 
-    # @check("exists")
-    # def startup(self):
-    #     """application starts up."""
-    #     self.override_lookup()
-    #     self.server_up()
-    #     res = requests.get(Finance.BASE_PATH)
-    #     if res.status_code != 200:
-    #         raise Error((res.status_code, 200))
-    #     self.server_down()
-    #
-    # @check("startup")
-    # def can_register(self):
-    #     """user can register for account."""
-    #     self.server_up()
-    #     self.register("check50user1", "crimson", "crimson", True)
-    #     self.server_down()
-    #
-    # @check("startup")
-    # def register_empty_field_fails(self):
-    #     """registration with an empty field fails."""
-    #     self.server_up()
-    #     self.register("", "crimson", "crimson", False)
-    #     self.register("check50user1", "", "crimson", False)
-    #     self.register("check50user1", "", "", False)
-    #     self.server_down()
-    #
-    # @check("startup")
-    # def register_password_mismatch_fails(self):
-    #     """registration with password mismatch fails."""
-    #     self.server_up()
-    #     self.register("check50user1", "thisiscs50", "crimson", False)
-    #     self.server_down()
-    #
-    # @check("startup")
-    # def register_reject_duplicate_username(self):
-    #     """registration rejects duplicate username."""
-    #     self.server_up()
-    #     self.register("check50user1", "crimson", "crimson", True)
-    #     self.register("check50user1", "crimson", "crimson", False)
-    #     self.server_down()
-    #
-    # @check("can_register")
-    # def quote_handles_invalid(self):
-    #     """quote handles invalid ticker symbol."""
-    #     self.server_up()
-    #     self.login("check50user1", "crimson")
-    #     self.quote("ZZZZ", False)
-    #     self.server_down()
-    #
-    # @check("can_register")
-    # def quote_handles_blank(self):
-    #     """quote handles blank ticker symbol."""
-    #     self.server_up()
-    #     self.login("check50user1", "crimson")
-    #     self.quote("", False)
-    #     self.server_down()
-    #
-    # @check("can_register")
-    # def quote_handles_valid(self):
-    #     """quote handles valid ticker symbol."""
-    #     self.server_up()
-    #     self.login("check50user1", "crimson")
-    #     self.quote("AAAA", True, value="28.00")
-    #     self.server_down()
-    #
-    # @check("can_register")
-    # def buy_handles_invalid(self):
-    #     """buy handles invalid ticker symbol."""
-    #     self.server_up()
-    #     self.login("check50user1", "crimson")
-    #     self.transaction("buy", "ZZZZ", "2", False)
-    #     self.server_down()
-    #
-    # @check("can_register")
-    # def buy_handles_incorrect_shares(self):
-    #     """buy handles fractional, negative, and non-numeric shares."""
-    #     self.server_up()
-    #     self.login("check50user1", "crimson")
-    #     self.transaction("buy", "AAAA", "-1", False)
-    #     self.transaction("buy", "AAAA", "1.5", False)
-    #     self.transaction("buy", "AAAA", "foo", False)
-    #     self.server_down()
-    #
-    # @check("can_register")
-    # def buy_handles_valid(self):
-    #     """buy handles valid purchase."""
-    #     self.server_up()
-    #     self.login("check50user1", "crimson")
-    #     self.transaction("buy", "AAAA", "4", True, value="112.00", remaining="9,?888.00")
-    #     self.server_down()
-    #
-    # @check("buy_handles_valid")
-    # def sell_handles_invalid(self):
-    #     """sell handles invalid ticker symbol."""
-    #     self.server_up()
-    #     self.login("check50user1", "crimson")
-    #     self.transaction("sell", "ZZZZ", "2", False)
-    #     self.server_down()
-    #
-    # @check("buy_handles_valid")
-    # def sell_handles_valid(self):
-    #     """sell handles valid sell."""
-    #     self.server_up()
-    #     self.login("check50user1", "crimson")
-    #     self.transaction("sell", "AAAA", "2", True, value="56.00", remaining="9,?944.00")
-    #     self.server_down()
-    #
-    # @check("sell_handles_valid")
-    # def history(self):
-    #     """history shows transactions."""
-    #     self.server_up()
-    #     self.login("check50user1", "crimson")
-    #     self.transaction("buy", "BBBB", "1", True)
-    #     self.driver.get("{}/history".format(Finance.BASE_PATH))
-    #     if not re.search("14.00", self.driver.page_source):
-    #         raise Error("Expected to see result of purchase in history.")
-    #     if not re.search("28.00", self.driver.page_source):
-    #         raise Error("Expected to see result of sale in history.")
+    @check("exists_tweets")
+    def cs50_tweets(self):
+        """cs50 tweets score correctly"""
+        self.include("positive-words.txt")
+        self.include("negative-words.txt")
+        self.override_get_user_timeline()
+        out = self.spawn("./tweets @cs50").stdout()
+        stripped = Sentiments.ansi_escape.sub('', out).strip()
+        if (stripped != "0 hello, world!"):
+            raise Error("did not return as expected")
+
+    @check("exists_tweets")
+    def beatles(self):
+        """beatles tweets score correctly"""
+        self.include("positive-words.txt")
+        self.include("negative-words.txt")
+        self.override_get_user_timeline()
+        tweets = self.spawn("./tweets @beatles").stdout().split("\n")
+        beatles = ["1 All you need is love","-1 Hey Jude, don't make it bad", "0 Oh, I believe in yesterday"]
+        count = 0
+        while count < 3:
+            stripped = Sentiments.ansi_escape.sub('', tweets[count]).strip()
+            if (stripped != beatles[count]):
+                raise Error("did not return {}").format(beatles[count])
+            count += 1
+
+    @check("exists_tweets")
+    def elphiethedog(self):
+        """elphiethedog tweets score correctly"""
+        self.include("positive-words.txt")
+        self.include("negative-words.txt")
+        self.override_get_user_timeline()
+        tweets = self.spawn("./tweets @elphiethedog").stdout().split("\n")
+        elphiethedog = ["0 I love you but I hate your vacuum","1 The mailman seems friendly and fun to frighten"]
+        count = 0
+        while count < 2:
+            stripped = Sentiments.ansi_escape.sub('', tweets[count]).strip()
+            if (stripped != elphiethedog[count]):
+                raise Error("did not return {}").format(elphiethedog[count])
+            count += 1
+
+
+    # @check("exists_tweets")
+    # def tweets_no_args(self):
+    #     """tweets handles lack of arg"""
+    #     self.include("positive-words.txt")
+    #     self.include("negative-words.txt")
+    #     self.spawn("./tweets").exit(1)
+
+    # @check("exists_tweets")
+    # def tweets_too_many_args(self):
+    #     """handles too many args"""
+    #     self.include("positive-words.txt")
+    #     self.include("negative-words.txt")
+    #     self.spawn("./tweets love hate").exit(1)
