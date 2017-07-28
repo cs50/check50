29490e0056479971956aa504d2f30ea9c196fe62
Chad Sharp <cmlsharp@umich.edu>
2017-07-28 00:25:30 -0400

    Updated water, mario structure

--- checks/cs50/2017/x/water/checks.py
+++ checks/cs50/2017/x/water/checks.py
@@ -1,17 +1,18 @@
 import os
-import re
 import sys
 
 sys.path.append(os.getcwd())
-from check50 import Checks, check
+from check50 import *
 
+
+@checks
 class Water(Checks):
 
     @check()
     def exists(self):
         """water.c exists."""
         super(Water, self).exists("water.c")
-    
+
     @check("exists")
     def compiles(self):
         """water.c compiles."""
@@ -20,22 +21,22 @@ class Water(Checks):
     @check("compiles")
     def test1(self):
         """1 minute equals 12 bottles."""
-        self.spawn("./water").stdin("1").stdout("^.*12.*$", 12)
+        self.spawn("./water").stdin("1").stdout(bottles(12), "12\n")
 
     @check("compiles")
     def test2(self):
         """2 minute equals 24 bottles."""
-        self.spawn("./water").stdin("2").stdout("^.*24.*$", 24)
+        self.spawn("./water").stdin("2").stdout(bottles(24), "24\n")
 
     @check("compiles")
     def test5(self):
         """5 minute equals 60 bottles."""
-        self.spawn("./water").stdin("5").stdout("^.*60.*$", 60).exit(0)
+        self.spawn("./water").stdin("5").stdout(bottles(60), "60\n").exit(0)
 
     @check("compiles")
     def test10(self):
         """10 minute equals 120 bottles."""
-        self.spawn("./water").stdin("10").stdout("^.*120.*$", 120).exit(0)
+        self.spawn("./water").stdin("10").stdout(bottles(120), "120\n").exit(0)
 
     @check("compiles")
     def test_reject_foo(self):
@@ -51,3 +52,7 @@ class Water(Checks):
     def test_reject_123abc(self):
         """rejects "123abc" minutes"""
         self.spawn("./water").stdin("123abc").reject()
+
+
+def bottles(num):
+    return "(^|[^\d]){}[^\d]".format(num)
