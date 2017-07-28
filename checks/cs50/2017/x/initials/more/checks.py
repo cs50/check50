aa87b823a45c0e3287a933124c47a5ee3b55715a
Chad Sharp <cmlsharp@umich.edu>
2017-07-28 09:25:33 -0400

    Add pset 2 checks

---
+++ checks/cs50/2017/x/initials/more/checks.py
@@ -0,0 +1,14 @@
+from ..less.checks import *
+
+
+class InitialsMore(InitialsLess):
+
+    @check("compiles")
+    def space_between(self):
+        """Outputs HJ for hailey       James"""
+        self.spawn("./initials").stdin("hailey       James", prompt=False).stdout(match("HJ"), "HJ\n").exit(0)
+
+    @check("compiles")
+    def space_before_after(self):
+        """Outputs HJ for     hailey James    """
+        self.spawn("./initials").stdin("    hailey James    ", prompt=False).stdout(match("HJ"), "HJ\n").exit(0)
