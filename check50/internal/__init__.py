383625157110e183cbcbee47c934ff36aa678e42
Chad Sharp <chad@cs50.harvard.edu>
2018-06-06 13:21:08 -0400

    Cleaned up API, fixed setup.py

---
+++ check50/internal/__init__.py
@@ -0,0 +1,2 @@
+from .register import *
+from .logger import *
--- check50/internal_api/__init__.py
+++
@@ -1,3 +0,0 @@
-from check50.internal_api.register import *
-from check50.internal_api.helper import *
-from check50.internal_api.logger import *
