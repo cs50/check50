6db832ee895ee77d766e00a07a8336c0d5a7a97c
Chad Sharp <chad@cs50.harvard.edu>
2018-06-21 13:58:16 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,14 +11,13 @@ setup(
         "Topic :: Utilities"
     ],
     description="This is check50, with which you can check solutions to problems for CS50.",
-    install_requires=["attrs", "bs4", "gitpython", "pexpect", "requests", "termcolor", "submit50>=2.4.5"],
+    install_requires=["attrs", "bs4", "gitpython", "pexpect", "pyyaml", "requests", "termcolor", "submit50>=2.4.5"],
     keywords=["check", "check50"],
     name="check50",
     packages=find_packages(),
     python_requires=">= 3.6",
     entry_points={
-        "console_scripts": ["check50=check50.__main__:main",
-                            "check50-setup=check50.__main__:setup_main"]
+        "console_scripts": ["check50=check50.__main__:main"]
     },
     url="https://github.com/cs50/check50",
     version="3.0.0"
