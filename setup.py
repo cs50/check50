438d504597194d2236a0f22b3fdf5f077503c31c
Chad Sharp <chad@cs50.harvard.edu>
2018-07-13 14:18:50 -0400

    finish adding internationalization infrastructure

--- setup.py
+++ setup.py
@@ -1,19 +1,28 @@
 from setuptools import find_packages, setup
+from setuptools.command.install import install
 
-import glob
-import os
-import subprocess
 
-def create_mo_files():
-    """Compiles .po files in local/LANG to .mo files and returns them as array of data_files"""
-    for prefix in glob.glob("locale/*/LC_MESSAGES/*.po"):
-        for _,_,files in os.walk(prefix):
-            for file in files:
-                po_file = Path(prefix) / po_file
-                mo_file = po_file.parent / po_file.stem + ".mo"
-                subprocess.call(["msgfmt", "-o", mo_file, po_file])
-
-create_mo_files()
+try:
+    from babel.messages import frontend as babel
+except ImportError:
+    cmdclass = {}
+else:
+    # https://stackoverflow.com/questions/40051076/babel-compile-translation-files-when-calling-setup-py-install
+    class InstallWithCompile(install):
+        def run(self):
+            compiler = babel.compile_catalog(self.distribution)
+            option_dict = self.distribution.get_option_dict("compile_catalog")
+            compiler.domain = [option_dict["domain"][1]]
+            compiler.directory = option_dict["directory"][1]
+            compiler.run()
+            super().run()
+    cmdclass = {
+        "compile_catalog": babel.compile_catalog,
+        "extract_messages": babel.extract_messages,
+        "init_catalog": babel.init_catalog,
+        "update_catalog": babel.update_catalog,
+        "install": InstallWithCompile
+    }
 
 setup(
     author="CS50",
@@ -26,7 +35,11 @@ setup(
     ],
     description="This is check50, with which you can check solutions to problems for CS50.",
     license="GPLv3",
-    install_requires=["attrs", "bs4", "pexpect", "push50", "pyyaml", "requests", "termcolor"],
+    cmdclass=cmdclass,
+    message_extractors = {
+        'check50': [('**.py', 'python', None),],
+    },
+    install_requires=["attrs", "babel", "bs4", "pexpect", "push50", "pyyaml", "requests", "termcolor"],
     extras_require = {
         "develop": ["sphinx", "sphinx_rtd_theme"]
     },
