e3503c91b4d1d378fdbb0ad73972ff624bb891ec
jelleas <jelle.van.assema@gmail.com>
2020-05-12 21:03:25 +0200

    find/replace --log

--- check50/c.py
+++ check50/c.py
@@ -130,4 +130,4 @@ def _check_valgrind(xml_file):
 
     # Only raise exception if we encountered errors.
     if reported:
-        raise Failure(_("valgrind tests failed; rerun with --log for more information."))
+        raise Failure(_("valgrind tests failed; see the web UI for more information."))
