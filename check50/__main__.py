d35f1dadc96dc22d4b3579188b9fba54a705038b
jelleas <jelle.van.assema@gmail.com>
2020-07-10 20:52:12 +0200

    wording

--- check50/__main__.py
+++ check50/__main__.py
@@ -304,7 +304,7 @@ def main():
                         help=_("file to write output to"))
     parser.add_argument("-v", "--verbose",
                         action="store_true",
-                        help=_("shows any installed dependencies and shows print statements written in checks"))
+                        help=_("shows which dependencies get installed (if any) and shows print statements in checks"))
     parser.add_argument("--log-level",
                         action="store",
                         default="warning",
