ba5e3417de542d41c8608d645e6c0e62dc7f4c9d
Kareem Zidane <kzidane@cs50.harvard.edu>
2021-07-16 16:18:45 -0400

    Deploy with GitHub Actions

--- setup.py
+++ setup.py
@@ -29,6 +29,6 @@ setup(
         "console_scripts": ["check50=check50.__main__:main"]
     },
     url="https://github.com/cs50/check50",
-    version="3.3.1",
+    version="3.3.2",
     include_package_data=True
 )
