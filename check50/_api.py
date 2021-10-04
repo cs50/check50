9efd934b7d8d7fd3e9a3b72a57d7ed5dfe17d377
Rongxin Liu <rongxinliu.dev@gmail.com>
2021-10-04 03:15:21 -0400

    raise FileNotFoundError in addition to IsADirectoryError in _copy

--- check50/_api.py
+++ check50/_api.py
@@ -515,7 +515,7 @@ def _copy(src, dst):
     """Copy src to dst, copying recursively if src is a directory."""
     try:
         shutil.copy(src, dst)
-    except IsADirectoryError:
+    except (FileNotFoundError, IsADirectoryError):
         if os.path.isdir(dst):
             dst = os.path.join(dst, os.path.basename(src))
         shutil.copytree(src, dst)
