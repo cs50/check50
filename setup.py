92644b5b72d69e903cfd24a7ee025aa874bae452
Brenda Anderson <curiouskiwi@users.noreply.github.com>
2023-12-24 13:35:08 +1300

    Update setup.py
    update version

--- setup.py
+++ setup.py
@@ -30,6 +30,6 @@ setup(
         "console_scripts": ["check50=check50.__main__:main"]
     },
     url="https://github.com/cs50/check50",
-    version="3.3.10",
+    version="3.3.11",
     include_package_data=True
 )
