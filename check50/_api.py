e62a35a7cf9e113e033bb24e4b59e8dec9cd3842
Brandon Nguyen <112731698+bxngyn@users.noreply.github.com>
2025-07-11 12:03:58 -0400

    Update _api.py

--- check50/_api.py
+++ check50/_api.py
@@ -463,7 +463,7 @@ class Mismatch(Failure):
         if actual == EOF:
             actual = "EOF"
 
-        self.payload.update({"expected": expected, "actual": actual})
+        self.payload.update({"expected": repr(expected), "actual": repr(actual)})
 
 
 def hidden(failure_rationale):
