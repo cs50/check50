0efa1169f9d8e9d3120d9d4c01fbcbd14e1d282e
ivanharvard <144486839+ivanharvard@users.noreply.github.com>
2025-07-23 15:31:53 -0400

    added warning about use in interactive mode

--- check50/__init__.py
+++ check50/__init__.py
@@ -17,11 +17,18 @@ def _setup_translation():
         "check50", str(files("check50").joinpath("locale")), fallback=True)
     _translation.install()
 
-
-
 # Encapsulated inside a function so their local variables/imports aren't seen by autocompleters
 _set_version()
 _setup_translation()
+
+# Discourage use of check50 in the interactive mode, due to a naming conflict of
+# the `_` variable. check50 uses it for translations, but Python stores the
+# result of the last expression in a variable called `_`.
+import sys
+if hasattr(sys, 'ps1') or sys.flags.interactive:
+    import warnings
+    warnings.warn(_("check50 is not intended for use in interactive mode."
+                    "Some behavior may not function as expected."))
 
 from ._api import (
     import_checks,
